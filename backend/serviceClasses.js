aebe89c300eca4c3d7f6bca8c510a36b7a950303
justincheung <info@dscsol.com>
2022-04-05 23:20:52 +0800

    update

--- backend/serviceClasses.js
+++ backend/serviceClasses.js
@@ -9,7 +9,7 @@ class Method {
   async addSubscriber(email) {
     let existEmail = await knex("subscriber").where("email", email);
     if (!existEmail[0]) {
-      knex("subscriber").insert({
+      await knex("subscriber").insert({
         email: email,
       });
     }
