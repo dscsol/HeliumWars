211e2139947490c3045e2827efe2bb75e0c1e391
justincheung <info@dscsol.com>
2022-04-19 13:26:08 +0800

    ok

--- smartContracts/contracts/NFTstaking.sol
+++ smartContracts/contracts/NFTstaking.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
 import "./Heliumwars.sol";
@@ -11,7 +12,8 @@ contract NFTstaking is IERC721Receiver, AccessControl {
     address public rewardTokenContract;
     bool public stakableState = false;
     bool public rewardClaimableState = false;
-    uint256 public MAX_REWARD_ALLOWED;
+    uint256 public rewardMaxSupply;
+    uint256 public totalRewardClaimed;
     uint256 public totalStakedNFT;
     uint256 public tokenPerStakingTime;
     uint256 constant tokenUnit = 10e18;
@@ -33,15 +35,9 @@ contract NFTstaking is IERC721Receiver, AccessControl {
     event Unstaked(address owner, uint256 tokenID);
     event RewardPaid(address owner, uint256 balance);
 
-    constructor(
-        Heliumwars _HeliumwarsNFTcontract,
-        address _rewardTokenContract,
-        uint256 _MAX_REWARD_ALLOWED,
-    ) {
+    constructor(Heliumwars _HeliumwarsNFTcontract) {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         HeliumwarsNFTcontract = Heliumwars(_HeliumwarsNFTcontract);
-        rewardTokenContract = _rewardTokenContract;
-        MAX_REWARD_ALLOWED = _MAX_REWARD_ALLOWED;
     }
 
     function setStakableState(bool newState) external onlyRole(ADMIN) {
@@ -49,9 +45,17 @@ contract NFTstaking is IERC721Receiver, AccessControl {
         emit stakableStateUpdated(newState);
     }
 
-    function setRewardClaimableState(bool newState) {
+    function setRewardClaimableState(bool newState) external onlyRole(ADMIN) {
         rewardClaimableState = newState;
         emit rewardClaimableStateUpdated(newState);
+    }
+
+    function setRewardTokenContract(address addr) external onlyRole(ADMIN) {
+        rewardTokenContract = addr;
+    }
+
+    function setRewardMaxSupply(uint256 amount) external onlyRole(ADMIN) {
+        rewardMaxSupply = amount;
     }
 
     function getStakedToken(address addr)
@@ -65,28 +69,20 @@ contract NFTstaking is IERC721Receiver, AccessControl {
     function getTokenStakingStartingTime(address addr, uint256 tokenID)
         external
         view
-        returns (uint256 memory tokenStakingStartingTime)
+        returns (uint256)
     {
-        return stakers[addr].tokenStakingStartingTime;
+        return stakers[addr].tokenStakingStartingTime[tokenID];
     }
 
-    function getBalance(address addr)
-        external
-        view
-        returns (uint256 memory rewardBalance)
-    {
+    function getBalance(address addr) external view returns (uint256) {
         return stakers[addr].rewardBalance;
     }
 
-    function getRewardClaimed(address addr)
-        external
-        view
-        returns (uint256 memory rewardClaimed)
-    {
+    function getRewardClaimed(address addr) external view returns (uint256) {
         return stakers[addr].rewardClaimed;
     }
 
-    function stake(uint256 _tokenID) external {
+    function stake(address addr, uint256 _tokenID) external {
         require(stakableState, "staking is not active");
         require(
             HeliumwarsNFTcontract.ownerOf(_tokenID) == msg.sender,
@@ -96,46 +92,41 @@ contract NFTstaking is IERC721Receiver, AccessControl {
         stakers[msg.sender].tokenStakingStartingTime[_tokenID] = block
             .timestamp;
         tokenOwner[_tokenID] = msg.sender;
-        HeliumwarsNFTcontract.safeTransferFrom(
-            msg.sender,
-            address(this),
-            _tokenID
-        );
+        HeliumwarsNFTcontract.safeTransferFrom(addr, address(this), _tokenID);
         emit Staked(msg.sender, _tokenID);
         totalStakedNFT++;
     }
 
-    function batchStake(uint256[] memory _tokenIDs) external {
+    function batchStake(address addr, uint256[] memory _tokenIDs) external {
         require(stakableState, "staking is not active");
         for (uint256 i = 0; i < _tokenIDs.length; i++) {
             require(
                 HeliumwarsNFTcontract.ownerOf(_tokenIDs[i]) == msg.sender,
                 "must be the owner of the NFT"
             );
-            stakers[msg.sender].tokenIDs.push(_tokenID[i]);
-            stakers[msg.sender].tokenStakingStartingTime[_tokenID[i]] = block
+            stakers[msg.sender].tokenIDs.push(_tokenIDs[i]);
+            stakers[msg.sender].tokenStakingStartingTime[_tokenIDs[i]] = block
                 .timestamp;
-            tokenOwner[_tokenID[i]] = msg.sender;
-            HeliumwarsNFTcontract.approve(address(this), _tokenID[i]);
+            tokenOwner[_tokenIDs[i]] = msg.sender;
+            HeliumwarsNFTcontract.approve(address(this), _tokenIDs[i]);
             HeliumwarsNFTcontract.safeTransferFrom(
-                msg.sender,
+                addr,
                 address(this),
-                _tokenID[i]
+                _tokenIDs[i]
             );
-            emit Staked(msg.sender, _tokenID[i]);
+            emit Staked(msg.sender, _tokenIDs[i]);
             totalStakedNFT++;
         }
     }
 
-    function unstake(uint256 _tokenID) external {
-        updateReward()
-        claimReward();
+    function unstake(address addr, uint256 _tokenID) external {
+        updateReward();
+        claimReward(addr);
 
         require(
             tokenOwner[_tokenID] == msg.sender,
             "must be the owner of the NFT"
         );
-
         uint256 index = indexOf(stakers[msg.sender].tokenIDs, _tokenID);
         stakers[msg.sender].tokenIDs[index] = stakers[msg.sender].tokenIDs[
             stakers[msg.sender].tokenIDs.length - 1
@@ -144,12 +135,9 @@ contract NFTstaking is IERC721Receiver, AccessControl {
         if (stakers[msg.sender].tokenIDs.length > 0) {
             stakers[msg.sender].tokenIDs.pop();
         }
+
         stakers[msg.sender].tokenStakingStartingTime[_tokenID] = 0;
-        HeliumwarsNFTcontract.safeTransferFrom(
-            address(this),
-            msg.sender,
-            _tokenID
-        );
+        HeliumwarsNFTcontract.safeTransferFrom(address(this), addr, _tokenID);
         delete tokenOwner[_tokenID];
         totalStakedNFT--;
         emit Unstaked(msg.sender, _tokenID);
@@ -162,9 +150,9 @@ contract NFTstaking is IERC721Receiver, AccessControl {
         }
     }
 
-    function batchUnstake(uint256[] memory _tokenIDs) external {
-        updateReward()
-        claimReward();
+    function batchUnstake(address addr, uint256[] memory _tokenIDs) external {
+        updateReward();
+        claimReward(addr);
 
         for (uint256 i = 0; i < _tokenIDs.length; i++) {
             require(
@@ -182,7 +170,7 @@ contract NFTstaking is IERC721Receiver, AccessControl {
             stakers[msg.sender].tokenStakingStartingTime[_tokenIDs[i]] = 0;
             HeliumwarsNFTcontract.safeTransferFrom(
                 address(this),
-                msg.sender,
+                addr,
                 _tokenIDs[i]
             );
             delete tokenOwner[_tokenIDs[i]];
@@ -197,48 +185,70 @@ contract NFTstaking is IERC721Receiver, AccessControl {
         }
     }
 
-    function claimReward() public {
+    function claimReward(address addr) public {
         require(rewardClaimableState, "unstake is not active");
         require(stakers[msg.sender].rewardBalance > 0, "0 rewards yet");
+        mint(addr, stakers[addr].rewardBalance);
         stakers[msg.sender].rewardClaimed += stakers[msg.sender].rewardBalance;
         stakers[msg.sender].rewardBalance = 0;
-
-        //mint
-
         emit RewardPaid(msg.sender, stakers[msg.sender].rewardBalance);
     }
 
     function updateReward() public {
         require(stakers[msg.sender].tokenIDs.length > 0, "no token staked");
-        stakers[msg.sender].rewardBalance = 0
+        stakers[msg.sender].rewardBalance = 0;
         for (uint256 i = 0; i < stakers[msg.sender].tokenIDs.length; i++) {
-            
             if (
-                stakers[msg.sender].tokenStakingStartingTime[stakers[msg.sender].tokenIDs[i]] <
-                block.timestamp  &&
-                stakers[msg.sender].tokenStakingStartingTime[stakers[msg.sender].tokenIDs[i]] > 0
+                stakers[msg.sender].tokenStakingStartingTime[
+                    stakers[msg.sender].tokenIDs[i]
+                ] <
+                block.timestamp &&
+                stakers[msg.sender].tokenStakingStartingTime[
+                    stakers[msg.sender].tokenIDs[i]
+                ] >
+                0
             ) {
                 uint256 stakedDays = (
                     (block.timestamp -
-                        uint256(stakers[msg.sender].tokenStakingStartingTime[stakers[msg.sender].tokenIDs[i]]))
+                        uint256(
+                            stakers[msg.sender].tokenStakingStartingTime[
+                                stakers[msg.sender].tokenIDs[i]
+                            ]
+                        ))
                 ) / stakingTimeUnit;
 
-                stakers[msg.sender].balance += token * stakedDays;
+                stakers[msg.sender].rewardBalance += tokenUnit * stakedDays;
             }
         }
     }
 
+    function mint(address addr, uint256 amount)
+        internal
+        returns (bool success)
+    {
+        require(
+            rewardMaxSupply > totalRewardClaimed + amount,
+            "reach max supply"
+        );
+        bytes memory payload = abi.encodeWithSignature(
+            "mint(address, uint256)",
+            addr,
+            amount
+        );
+        address(rewardTokenContract).call(payload);
+        totalRewardClaimed += amount;
+        return true;
+    }
+
     function onERC721Received(
-        address operator,
-        address from,
-        uint256 tokenId,
-        bytes calldata data
-    ) external returns (bytes4) {
+        address,
+        address,
+        uint256,
+        bytes calldata
+    ) external pure override returns (bytes4) {
         return
             bytes4(
-                keccak256(
-                    "onERC721Received(address operator, address from, uint256 tokenId, bytes data)"
-                )
+                keccak256("onERC721Received(address,address,uint256,bytes)")
             );
     }
 
